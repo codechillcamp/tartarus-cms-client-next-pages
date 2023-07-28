fac6fb863ffd5b56830bb442e89518c2e01af7b2
Leonss23 <103018008+Leonss23@users.noreply.github.com>
2023-07-28 01:34:52 -0300

    chore: set up initial pages/components, configs and deps

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from '@/components/layout/header';
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { Inter } from 'next/font/google';
+import { ThemeProvider as NextThemeProvider } from 'next-themes';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+      >
+        <div className={inter.className}>
+          <Header />
+          <Component {...pageProps} />
+        </div>
+      </NextThemeProvider>
+    </>
+  );
 }
